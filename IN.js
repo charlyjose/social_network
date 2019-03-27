97ed89cc9d59a57e0684883053e41db153bdf090
charlyjose <charlyjose@github.com>
2019-03-27 11:45:14 +0530

    Added Routes

--- IN.js
+++
@@ -1,8 +0,0 @@
-
-// Change your API keys received from developer.linkedin.com below
-var APIKey = "XXXXXXXXXXXXXXXX";
-var APIKeySecret = "XXXXXXXXXXXXXXXXXXXX";
-var callbackURL = "http://localhost:3000/verify/";
-var APIVersion = "v1";
-
-var APIScope = 'r_basicprofile';
--- app.js
+++ app.js
@@ -12,11 +12,20 @@ var appCSS = require('./routes/appCSS');
 var favic = require('./routes/favic');
 var usnLogo = require('./routes/usnLogo');
 
-
 var indexRouter = require('./routes/index');
 var signupRouter = require('./routes/signup');
 var loginRouter = require('./routes/login');
 var homeRouter = require('./routes/home');
+
+var forgotPasswordRouter = require('./routes/forgotPassword');
+var resetPasswordRouter = require('./routes/resetPassword');
+var profileRouter = require('./routes/profile');
+var resetUserRouter = require('./routes/resetUser');
+var resetPassRouter = require('./routes/resetPass');
+var resetContactsRouter = require('./routes/resetContacts');
+var deleteAccRouter = require('./routes/deleteAcc');
+
+
 
 
 //var inSigninRouter = require('./routes/INSignIn');
@@ -42,18 +51,19 @@ app.use('/app.css', appCSS);
 app.use('/favicon.ico', favic);
 app.use('/usnlogo.jpeg', usnLogo);
 
-
 app.use('/', indexRouter);
+app.use('/home', homeRouter);
+app.use('/profile', profileRouter);
+
 app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
 app.use('/signin', loginRouter);
-app.use('/forgotpassword', forgotPassword);
-app.use('/reset-password', resetPassword);
-app.use('/home', homeRouter);
-app.use('/profile', profileRouter);
+
+app.use('/forgotpassword', forgotPasswordRouter);
+app.use('/reset-password', resetPasswordRouter);
 app.use('/reset-username', resetUserRouter);
 app.use('/reset-password', resetPassRouter);
-app.use('/reset-contacts', resetContRouter);
+app.use('/reset-contacts', resetContactsRouter);
 app.use('/delete-account', deleteAccRouter);
 
 
