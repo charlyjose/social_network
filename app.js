75dc830d69cd8811a38eeeddf04cb0c287777dc9
Charly Jose <charleyjoz2011@gmail.com>
2019-03-27 07:25:28 +0530

    Added Error Pages

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql');
 
+
+
+var appJS = require('./routes/appJS');
+var appCSS = require('./routes/appCSS');
+var favic = require('./routes/favic');
+
 var indexRouter = require('./routes/index');
 var signupRouter = require('./routes/signup');
 var loginRouter = require('./routes/login');
-var inSigninRouter = require('./routes/INSignIn');
 var homeRouter = require('./routes/home');
+
+
+
+//var inSigninRouter = require('./routes/INSignIn');
+
+
 
 var app = express();
 
@@ -23,12 +34,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+app.use('/app.js', appJS);
+app.use('/app.css', appCSS);
+app.use('/favicon.ico', favic);
+
+
 app.use('/', indexRouter);
 app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
 app.use('/signin', loginRouter);
+app.use('/forgotpassword', forgotPassword);
+app.use('/reset-password', resetPassword);
+app.use('/home', homeRouter);
+app.use('/profile', profileRouter);
+app.use('/reset-username', resetUserRouter);
+app.use('/reset-password', resetPassRouter);
+app.use('/reset-contacts', resetContRouter);
+app.use('/delete-account', deleteAccRouter);
+
+
+/*
 app.use('/in', inSigninRouter);
 app.use('/in/verified', homeRouter);
+*/
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
